d028fb22d8fc81849d92525c2a4955e9612fd7ce
Jeff Kwan <informaverse@gmail.com>
2023-08-14 16:27:00 +0800

    setting up api call

---
+++ test.py
@@ -0,0 +1,18 @@
+import openai
+import os
+
+# Set the API key
+from dotenv import load_dotenv
+load_dotenv()
+# Retrieve the API key
+api_key = os.environ.get('OPENAI_API_KEY')
+print(api_key)
+
+'''
+openai.api_key = os.environ['OPENAI_API_KEY']
+# Use the ChatGPT model to generate text
+model_engine = 'text-davinci-002'
+prompt = 'Hello, how are you today?'
+completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1,stop=None,temperature=0.7)
+message = completion.choices[0].text
+print(message)'''
