fc8367f799c2ba43511f7c7df23a5e671b758e3a
Jeff Kwan <informaverse@gmail.com>
2023-09-03 16:06:24 +0800

    update

--- src/context.py
+++ src/context.py
@@ -9,6 +9,12 @@ import os
 import json
 import sys
 
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.llm import LLMChain
+from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+
+
 def display_progress_bar(iteration, total, bar_length=50):
     progress = (iteration / total)
     arrow = '-' * int(round(progress * bar_length) - 1) + '>'
@@ -20,38 +26,48 @@ def display_progress_bar(iteration, total, bar_length=50):
 # Content Extraction Class
 class ContentExtractor:
     def __init__(self, directory):
-        self.directory = directory
+        self.context_dir = directory
 
 
     def extract_content(self, dir_name):
-        total_files = sum([len(files) for subdir, _, files in os.walk(self.directory)])
+        total_files = sum([len(files) for subdir, _, files in os.walk(self.context_dir)])
         processed_files = 0
 
         # Ensure the data directory and subdirectory exist
-        os.makedirs(os.path.join('src/data', dir_name), exist_ok=True)
-        json_path = os.path.join('src/data', dir_name, 'context.json')
-        
-        # Create or reset the file at the beginning
-        with open(json_path, 'w') as json_file:
+        self.data_dir = os.path.join('src/data', dir_name)
+        os.makedirs(self.data_dir, exist_ok=True)
+        context_dir = os.path.join(self.data_dir, 'context.json')
+
+        # Open the JSON file and write the opening curly brace
+        with open(context_dir, 'w') as json_file:
             json_file.write("{\n")
-        
-        for subdir, _, files in os.walk(self.directory):  # Recursively walk through all files and subdirectories
+
+        first_entry = True
+
+        for subdir, _, files in os.walk(self.context_dir):  # Recursively walk through all files and subdirectories
             for file in files:
                 file_path = os.path.join(subdir, file)
                 file_content = self.extract_file(file_path)
-                if file_content:
-                    with open(json_path, 'a') as json_file:
-                        json.dump({file_path: file_content}, json_file)
-                        json_file.write(",\n")  # Preparing for the next record
-                    
+
+                # Construct the JSON entry for this file
+                json_entry = json.dumps({file_path: file_content}, indent=4)
+
+                # If this isn't the first entry, prepend a comma
+                if not first_entry:
+                    json_entry = ",\n" + json_entry
+                else:
+                    first_entry = False
+
+                # Append the JSON entry to the file
+                with open(context_dir, 'a') as json_file:
+                    json_file.write(json_entry)
+
                 processed_files += 1
                 display_progress_bar(processed_files, total_files)
 
-        # Closing the last comma and enclosing the JSON content
-        with open(json_path, 'rb+') as file:
-            file.seek(-2, os.SEEK_END)  # Seek the position to the last comma
-            file.truncate()  # Remove the last comma
-            file.write(b"\n}")  # Close the JSON content
+        # Write the closing curly brace to the JSON file
+        with open(context_dir, 'a') as json_file:
+            json_file.write("\n}")
 
 
     def extract_file(self, file_path):
@@ -78,15 +94,44 @@ class ContentExtractor:
                 text = file.read()
 
         return text
+    
+
+    def extract_summary(self):
+        # Load the detailed_context.json file
+        with open(os.path.join(self.data_dir, 'context.json'), 'r') as file:
+            detailed_context = json.load(file)
+
+        summarized_context = {}
+
+        for key, content in detailed_context.items():
+            summarized_context[key] = self._summarize_map_reduce(content)
+
+        # Save the summarized content to summarized_context.json
+        with open(os.path.join(self.data_dir, 'summaries.json'), 'w') as file:
+            json.dump(summarized_context, file, indent=4)
+
+    def _summarize_map_reduce(self, content):
+        prompt_template = """Write a concise summary of the following:
+        "{text}"
+        CONCISE SUMMARY:"""
+        prompt = PromptTemplate.from_template(prompt_template)
+
+        llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+        llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+        stuff_chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
+
+        return stuff_chain.run(content)
         
 
 if __name__ == '__main__':
     from time import time
     start = time()
-    print('\nStart Content Extraction')
+    print('\nStart Context.py')
 
     Extractor = ContentExtractor(r'C:\Users\infor\OneDrive\Jeff\Cambridge\IIB Project - RNN\Readings')
-    Extractor.extract_content('RNN Readings')
+    Extractor.data_dir = r'C:\Users\infor\Desktop\gpt-mind\src\data\RNN_Readings'
+    Extractor.extract_summary()
 
     print(f'\nRuntime = {round(time()-start,1)}')
 
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import openai
 import os
-
+'''
 # Set the API key
 from dotenv import load_dotenv
 load_dotenv()
@@ -13,4 +13,15 @@ prompt = 'Hello, how are you today?'
 completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1,stop=None,temperature=0.7)
 message = completion.choices[0].text
 print(message)
+'''
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import WebBaseLoader
+from langchain.chains.summarize import load_summarize_chain
 
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+docs = loader.load()
+
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+chain = load_summarize_chain(llm, chain_type="stuff")
+
+chain.run(docs)
